912a028cb221adf22b79b5abe8d53fc2240d80cc
paawan-web-ai <matrixescape345@gmail.com>
2026-02-21 15:59:31 +0530

    Delete day-8 directory

--- day-8/server.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-const app = require("./src/app")
-const connectToDb = require("./src/config/database")
-
-connectToDb()
-
-app.listen(3000,()=>{
-    console.log("server 3000 is running")
-})
