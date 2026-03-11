fe942d05300312ce002ce457fd834705a44917ec
paawan-web-ai <matrixescape345@gmail.com>
2026-03-11 17:38:38 +0530

    Delete multer/Backend directory

--- multer/Backend/src/config/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-function connectToDb() {
-  mongoose.connect(process.env.MONGO_URI).then(() => {
-    console.log("Connect To DB");
-  });
-}
-module.exports = connectToDb;
