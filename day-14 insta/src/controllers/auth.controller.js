058a8c4feb417f3862440985c07312fdcc182ad9
paawan-web-ai <matrixescape345@gmail.com>
2026-03-20 21:59:31 +0530

    bcrypt

--- day-14 insta/src/controllers/auth.controller.js
+++ day-14 insta/src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const userModel = require("../models/user.model");
-const crypto = require("crypto");
+// const crypto = require("crypto");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 async function registerController(req, res) {
@@ -19,7 +20,7 @@ async function registerController(req, res) {
     });
   }
 
-  const hash = crypto.createHash("sha256").update(password).digest("hex");
+  const hash = await bcrypt.hash(password, 10);
 
   const user = await userModel.create({
     username,
@@ -70,9 +71,9 @@ async function loginController(req, res) {
       message: "user not found",
     });
   }
-  const hash = crypto.createHash("sha256").update(password).digest("hex");
 
-  const isPasswordValid = hash == user.password;
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
   if (!isPasswordValid) {
     return res.status(404).json({
       message: "password invalid",
