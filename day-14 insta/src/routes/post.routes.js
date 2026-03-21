1683c557a0e6b048c84d48cb8a2e30b87317428e
paawan-web-ai <matrixescape345@gmail.com>
2026-03-21 21:32:23 +0530

    image

--- day-14 insta/src/routes/post.routes.js
+++ day-14 insta/src/routes/post.routes.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const postRouter = express.Router();
 const postController = require("../controllers/post.controller");
+const multer = require("multer");
+const upload = multer({ storage: multer.memoryStorage() });
+
 /*
 -POST /api/posts [protected]
 -req.body = {caption , image-file}
 */
-postRouter.post("/", postController.createPostController);
+postRouter.post(
+  "/",
+  upload.single("image"),
+  postController.createPostController,
+);
 
 module.exports = postRouter;
