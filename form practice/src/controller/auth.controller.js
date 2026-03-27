cd6f18a3b55fbc0a4b790cf74b5172f24eb843c9
paawan-web-ai <matrixescape345@gmail.com>
2026-03-27 15:06:31 +0530

    login/register form

---
+++ form practice/src/controller/auth.controller.js
@@ -0,0 +1,94 @@
+const userModel = require("../models/user.models");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+async function registerController(req, res) {
+  const { username, email, password } = req.body;
+
+  const isUserAlreadyExists = await userModel.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (isUserAlreadyExists) {
+    const isEmailTaken = isUserAlreadyExists.email === email;
+    const isUsernameTaken = isUserAlreadyExists.username === username;
+
+    let message = "Registration failed: ";
+    if (isEmailTaken && isUsernameTaken) {
+      message += "Both Email and Username are already in user.";
+    } else if (isEmailTaken) {
+      message += "This Email is already registered";
+    } else {
+      message += "This Username is already taken";
+    }
+    return res.status(409).json({
+      message,
+    });
+  }
+
+  const hash = await bcrypt.hash(password, 10);
+
+  const user = await userModel.create({
+    username,
+    email,
+    password: hash,
+  });
+
+  const token = jwt.sign(
+    {
+      user: user._id,
+    },
+    process.env.JWT_SECRET,
+  );
+
+  res.cookie("jwt_token", token);
+
+  res.status(201).json({
+    message: "user registered",
+    user,
+    token,
+  });
+}
+
+async function loginController(req, res) {
+  const { identifier, password } = req.body;
+
+  const user = await userModel.findOne({
+    $or: [{ username: identifier }, { email: identifier }],
+  });
+
+  if (!user) {
+    return res.status(404).json({
+      message: "User Not Found!",
+    });
+  }
+
+  const ispasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!ispasswordValid) {
+    return res.status(404).json({
+      message: "Password Invalid!",
+    });
+  }
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "1d" },
+  );
+
+  res.cookie("token", token);
+
+  res.status(200).json({
+    message: "User LoggedIn Successfully",
+    user: {
+      username: user.username,
+      email: user.email,
+    },
+  });
+}
+module.exports = {
+  registerController,
+  loginController,
+};
---
+++ form practice/src/routes/auth.routes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const authRoutes = express.Router();
+const authController = require("../controller/auth.controller");
+
+authRoutes.post("/register", authController.registerController);
+authRoutes.post("/login", authController.loginController);
+module.exports = authRoutes;
