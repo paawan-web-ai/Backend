ccf2c50954c7282005e2b9e936f10f2ea82dfa1f
paawan-web-ai <matrixescape345@gmail.com>
2026-04-03 13:03:17 +0530

    post_get_by_id

--- day-14 insta/src/controllers/post.controller.js
+++ day-14 insta/src/controllers/post.controller.js
@@ -2,6 +2,7 @@ const postModel = require("../models/post.model");
 const ImageKit = require("@imagekit/nodejs");
 const { toFile } = require("@imagekit/nodejs");
 const jwt = require("jsonwebtoken");
+
 const imagekit = new ImageKit({
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
 });
@@ -45,6 +46,81 @@ async function createPostController(req, res) {
   });
 }
 
+async function getPostController(req, res) {
+  const token = req.cookies.token;
+  // to read token's data
+
+  if (!token) {
+    return res.status(401).json({
+      message: "unAuthorized Access",
+    });
+  }
+
+  let decoded = null;
+  try {
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (err) {
+    return res.status(401).json({
+      message: "Token invalid",
+    });
+  }
+  const userId = decoded.id;
+
+  const posts = await postModel.find({
+    user: userId,
+  });
+
+  res.status(200).json({
+    message: "Posts fetched successfully",
+    posts,
+  });
+}
+
+async function getPostDetails(req, res) {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({
+      message: "unAuthorized Access",
+    });
+  }
+
+  let decoded;
+
+  try {
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (err) {
+    return res.status(401).json({
+      message: "Invalid token",
+    });
+  }
+
+  const userID = decoded.id;
+  const postID = req.params.postID;
+  const postdetails = await postModel.findById(postID);
+
+  if (!postdetails) {
+    return res.status(404).json({
+      message: "post not found.",
+    });
+  }
+
+  const isValidUser = postdetails.user.toString() === userID;
+
+  if (!isValidUser) {
+    return res.status(403).json({
+      message: "forbidden content.",
+    });
+  }
+
+  return res.status(200).json({
+    message: "post fetched successfully.",
+    postdetails,
+  });
+}
+
 module.exports = {
   createPostController,
+  getPostController,
+  getPostDetails,
 };
--- day-14 insta/src/routes/post.routes.js
+++ day-14 insta/src/routes/post.routes.js
@@ -14,4 +14,17 @@ postRouter.post(
   postController.createPostController,
 );
 
+/*
+ Get /api/posts/[protected]
+ */
+
+postRouter.get("/", postController.getPostController);
+
+/*
+GET / api/posts/details/:postid
+- return an detail about specific post with the id . also check whether the post belongs to the user that the request come from
+ */
+
+postRouter.get("/details/:postID", postController.getPostDetails);
+
 module.exports = postRouter;
