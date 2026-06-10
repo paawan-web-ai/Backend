00c0eb5aa9d2abcd224a0e0672a26a73ab87b196
paawan-web-ai <matrixescape345@gmail.com>
2026-06-10 23:07:53 +0530

    createpost

--- day-14 insta/Frontend/src/features/post/pages/CreatePost.jsx
+++ day-14 insta/Frontend/src/features/post/pages/CreatePost.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-
+import React, { useRef, useState } from 'react'
+import "../pages/Feed.scss"
 const CreatePost = () => {
+
+    const [caption, setcaption] = useState("")
+    const postImageInputFieldRef = useRef(null)
+
+    function handleSubmit(e) {
+        e.preventDefault()
+
+        const file = postImageInputFieldRef.current.files[0]
+    }
+
     return (
-        <div>
-            createPost
-        </div>
+        <main className="create-post-page">
+            <div className="form-container">
+                <h1>Create Post</h1>
+                <form onSubmit={handleSubmit}>
+
+                    <label htmlFor="postImage">Select Image</label>
+                    <input hidden type="file" name="postImage" id='postImage' />
+                    <input type="text" name='caption' id='caption' placeholder='enter caption' />
+                    <button className='button' >createPost</button>
+                </form>
+            </div>
+        </main>
     )
 }
 
